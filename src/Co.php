659ea8424671ea571461bf2a7f4ff74b9058bc85
yicsunny <yicsunny@163.com>
2019-09-24 17:12:55 +0800

    Create Co.php
    12312

---
+++ src/Co.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zhang
+ * Date: 2019/9/24
+ * Time: 17:11
+ */
+namespace yicsunny\task;
+class Co
+{
+    public function index()
+    {
+        echo 111;
+    }
+
+
+}
